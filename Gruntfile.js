1cd1d974acb467526b8ab513a632b5f13a541e78
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-01-20 13:37:30 -0500

    passed grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,6 @@
 /*global module:false*/
 module.exports = function (grunt) {
-  var sourceFiles = ['index.js'];
-  var testFiles = ['test/*-spec.js'];
-  var globalName = 'selfAddressed';
+  var sourceFiles = ['script-attributes.js'];
 
   grunt.initConfig({
 
@@ -10,13 +8,11 @@ module.exports = function (grunt) {
       options: {
         valid: 'dashes'
       },
-      src: sourceFiles,
-      test: testFiles
+      src: sourceFiles
     },
 
     jshint: {
       all: sourceFiles,
-      test: testFiles,
       options: {
         jshintrc: 'utils/.jshintrc',
         reporter: require('jshint-summary')
@@ -38,50 +34,14 @@ module.exports = function (grunt) {
       }
     },
 
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec'
-        },
-        src: testFiles
-      }
-    },
-
-    watch: {
-      options: {
-        atBegin: true
-      },
-      all: {
-        files: [sourceFiles, testFiles],
-        tasks: ['test', 'lint']
-      }
-    },
-
     'clean-console': {
       options: {
         timeout: 1 // seconds to wait for any errors
       },
       iframe: {
         options: {
-          url: 'test/iframe/index.html'
+          url: 'index.html'
         }
-      },
-      'iframe-with-extra-data': {
-        options: {
-          url: 'test/iframe-with-extra-data/index.html'
-        }
-      }
-    },
-
-    browserify: {
-      src: {
-        options: {
-          browserifyOptions: {
-            standalone: globalName
-          }
-        },
-        src: ['index.js'],
-        dest: 'dist/self-addressed.js'
       }
     }
   });
@@ -90,6 +50,6 @@ module.exports = function (grunt) {
   plugins.forEach(grunt.loadNpmTasks);
 
   grunt.registerTask('lint', ['filenames', 'jshint', 'eslint', 'jscs']);
-  grunt.registerTask('test', ['mochaTest', 'clean-console']);
-  grunt.registerTask('default', ['deps-ok', 'nice-package', 'lint', 'sync', 'browserify', 'test']);
+  grunt.registerTask('test', ['clean-console']);
+  grunt.registerTask('default', ['deps-ok', 'nice-package', 'lint', 'sync', 'test']);
 };
